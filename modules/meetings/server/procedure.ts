b3b3cf2595876ba9e5b40fe43b59e76d3aff18c4
Sajtine <justinegomez777@gmail.com>
2026-01-11 00:55:34 +0800

    Done with the Meetings Form

--- modules/meetings/server/procedure.ts
+++ modules/meetings/server/procedure.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import { meetings } from "@/db/schema";
 import { eq, getTableColumns, and, ilike, desc, count } from "drizzle-orm";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
 
 import {
   createTRPCRouter,
@@ -17,7 +18,41 @@ import {
 } from "@/constant";
 
 export const meetingsRouter = createTRPCRouter({
-    
+  update: protectedProcedure
+    .input(meetingsUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [updatedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!updatedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return updatedMeeting;
+    }),
+  create: protectedProcedure
+    .input(meetingsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdMeeting] = await db
+        .insert(meetings)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      // TODO: Create Stream Call, Upsert Stream Users
+
+      return createdMeeting;
+    }),
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input, ctx }) => {
@@ -28,15 +63,15 @@ export const meetingsRouter = createTRPCRouter({
         })
         .from(meetings)
         .where(
-          and(
-            eq(meetings.id, input.id),
-            eq(meetings.userId, ctx.auth.user.id),
-          )
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
         );
 
-        if (!existingMeeting) {
-          throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found"})
-        }
+      if (!existingMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
 
       // await new Promise((resolve) => setTimeout(resolve, 5000));
       // throw new TRPCError({ code: "BAD_REQUEST" });
@@ -76,17 +111,16 @@ export const meetingsRouter = createTRPCRouter({
         .offset((page - 1) * pageSize);
 
       const [total] = await db
-      .select({ count: count() })
-      .from(meetings)
-      .where(
-        and(
+        .select({ count: count() })
+        .from(meetings)
+        .where(
+          and(
             eq(meetings.userId, ctx.auth.user.id),
             search ? ilike(meetings.name, `%${search}%`) : undefined
-        )
-      )
+          )
+        );
 
       const totalPages = Math.ceil(total.count / pageSize);
-      
 
       // await new Promise((resolve) => setTimeout(resolve, 5000));
       // throw new TRPCError({ code: "BAD_REQUEST" });
@@ -94,8 +128,7 @@ export const meetingsRouter = createTRPCRouter({
       return {
         items: data,
         total: total.count,
-        totalPages
-      }
+        totalPages,
+      };
     }),
-
 });
